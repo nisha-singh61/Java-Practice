0f9bdb3cf55e6dacc20dbcc1d9d23bb7fdc7bb09
Nisha <nishakuswaha2002@gmail.com>
2026-03-22 19:56:25 +0530

    String concat2

--- src/basics/HelloUser.java
+++ src/basics/HelloUser.java
@@ -2,7 +2,7 @@ package basics;
 
 public class HelloUser {
     public static void main(String[] args) {
-        String name = "Nisha"
+        String name = "Nisha";
         System.out.println("Hello " +name);
     }
 }
---
+++ src/basics/StringConcat.java
@@ -0,0 +1,9 @@
+package basics;
+
+public class StringConcat {
+    public static void main(String[] args) {
+        String firstName= "Nisha ";
+        String lastName= "Singh";
+        System.out.println(firstName+lastName);
+    }
+}
