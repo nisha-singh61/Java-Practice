9c2a3f76de9c2150474e6ef38cc70070bc16a7ae
Nisha <nishakuswaha2002@gmail.com>
2026-03-23 19:50:00 +0530

    Comments in java

---
+++ src/basics/CommentsJava.java
@@ -0,0 +1,9 @@
+package basics;
+
+public class CommentsJava {
+    public static void main() {
+        //This is a single line comment in java
+        //This is a multi-line comment in java
+        System.out.println("Hi!");
+    }
+}
