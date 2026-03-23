5befece6c87f9f209f520c1da3a4174c81c382de
Nisha <nishakuswaha2002@gmail.com>
2026-03-23 19:51:40 +0530

    added comment

--- src/basics/Printing.java
+++ src/basics/Printing.java
@@ -4,8 +4,11 @@ import java.sql.SQLOutput;
 
 public class Printing {
     public static void main() {
+        //using print()
         System.out.print("Hello JAVA....");
         System.out.print(" How are you? ");
+
+        //using println()
         System.out.println("I am Nisha");
         System.out.println("Up here to learn java!");
     }
