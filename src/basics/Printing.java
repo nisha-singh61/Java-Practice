2ca31fa86a2634556be28cd8b5ed5b9dc57b0dee
Nisha <nishakuswaha2002@gmail.com>
2026-03-23 19:46:59 +0530

    print and println in java

---
+++ src/basics/Printing.java
@@ -0,0 +1,12 @@
+package basics;
+
+import java.sql.SQLOutput;
+
+public class Printing {
+    public static void main() {
+        System.out.print("Hello JAVA....");
+        System.out.print(" How are you? ");
+        System.out.println("I am Nisha");
+        System.out.println("Up here to learn java!");
+    }
+}
