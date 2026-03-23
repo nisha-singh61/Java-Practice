0387df037f86bb4bba445580b3e0b6ff37ffaaf2
Nisha <nishakuswaha2002@gmail.com>
2026-03-23 19:40:21 +0530

    Printing Numbers

---
+++ src/basics/PrintNumbers.java
@@ -0,0 +1,9 @@
+package basics;
+
+public class PrintNumbers {
+   public static void main() {
+       System.out.println(99);
+       System.out.println(4875);
+       System.out.println(3657859);
+    }
+}
