fda48c69321613d04ce10285f8375b420da316c4
Nisha <nishakuswaha2002@gmail.com>
2026-03-24 20:22:37 +0530

    Same type of variable

---
+++ src/basics/SameTypeVar.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class SameTypeVar {
+    public static void main() {
+        short x = 50;
+        short y = 50;
+        short z = 50;
+        System.out.println(x+y+z);
+    }
+}
