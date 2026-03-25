f7320a3768faba0dbed6fb11a86bfaae8725a990
Nisha <nishakuswaha2002@gmail.com>
2026-03-25 20:22:01 +0530

    Multi-line comments

--- src/basics/CommentsJava.java
+++ src/basics/CommentsJava.java
@@ -3,7 +3,6 @@ package basics;
 public class CommentsJava {
     public static void main() {
         //This is a single line comment in java
-        //This is a multi-line comment in java
         System.out.println("Hi!");
     }
 }
---
+++ src/basics/MultiLineComments.java
@@ -0,0 +1,10 @@
+package basics;
+
+public class MultiLineComments {
+    public static void main() {
+        /* This is a
+        multi-line comment
+         */
+        System.out.println("Hi again!");
+    }
+}
