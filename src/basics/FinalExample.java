b366d23cb6c19d8ec131edd29dca4c8ea75451c3
Nisha <nishakuswaha2002@gmail.com>
2026-03-25 20:14:36 +0530

    Final keyword example

---
+++ src/basics/FinalExample.java
@@ -0,0 +1,9 @@
+package basics;
+
+public class FinalExample {
+    public static void main() {
+        final short myNum = 15;
+        // myNum = 20;   gives error
+        System.out.println(myNum);
+    }
+}
