8c439cf52ddaf01aac98158ea1190c589a4df1a4
Nisha <nishakuswaha2002@gmail.com>
2026-03-28 21:05:00 +0530

    Plus Operator

---
+++ src/basics/operator/arithematic/PlusOperator.java
@@ -0,0 +1,12 @@
+package basics.operator.arithematic;
+
+public class PlusOperator {
+    public static void main(String[] args) {
+        //declaring integer variables
+        int x = 100, y = 20, add;
+
+        add = x+y;
+
+        System.out.println("Sum of x and y is: "+add);
+    }
+}
