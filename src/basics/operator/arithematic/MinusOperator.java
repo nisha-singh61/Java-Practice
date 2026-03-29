ced7db34a68a69956276c9492e9540b3e899619d
Nisha <nishakuswaha2002@gmail.com>
2026-03-29 19:12:13 +0530

    Minus Operator with same variable type

---
+++ src/basics/operator/arithematic/MinusOperator.java
@@ -0,0 +1,9 @@
+package basics.operator.arithematic;
+
+public class MinusOperator {
+    public static void main() {
+        int x = 10 , y = 5, subtraction;
+        subtraction = x - y;
+        System.out.println("The value of x - y is " +subtraction);
+    }
+}
