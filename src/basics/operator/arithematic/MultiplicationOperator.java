d05b35521018d6945cfcb120a12faa919b1f22b3
Nisha <nishakuswaha2002@gmail.com>
2026-03-30 20:34:25 +0530

    Multiplication operator

---
+++ src/basics/operator/arithematic/MultiplicationOperator.java
@@ -0,0 +1,9 @@
+package basics.operator.arithematic;
+
+public class MultiplicationOperator {
+    public static void main(String[] args) {
+        int x = 100, y = 20, multiply;
+        multiply = x*y;
+        System.out.println("Multiplication of x and y is: "+multiply);
+    }
+}
