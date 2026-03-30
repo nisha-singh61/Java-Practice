4263c6f4415db9a7e192efb6a990336a8dce0b1a
Nisha <nishakuswaha2002@gmail.com>
2026-03-30 20:41:36 +0530

    Multiplication operator on float and int variable

---
+++ src/basics/operator/arithematic/MultiplicationOperator2.java
@@ -0,0 +1,10 @@
+package basics.operator.arithematic;
+
+public class MultiplicationOperator2 {
+    public static void main(String[] args) {
+        int x = 100;
+        float y = 5.5f, multiply;
+        multiply = x*y;
+        System.out.println("Multiplication of x and y is: "+multiply);
+    }
+}
