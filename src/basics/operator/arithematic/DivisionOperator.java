764e57688edc88a4c9d3ce525b4a465b9a90accd
Nisha <nishakuswaha2002@gmail.com>
2026-03-30 20:43:02 +0530

    Division Operator

---
+++ src/basics/operator/arithematic/DivisionOperator.java
@@ -0,0 +1,12 @@
+package basics.operator.arithematic;
+
+public class DivisionOperator {
+    public static void main(String[] args) {
+        int x = 100, y = 20, division;
+
+        //The '/' arithmetic operator divides the variable x by
+        //variable y and returns the quotient
+        division = x/y;
+        System.out.println("Quotient value after dividing x by y: "+division);
+    }
+}
