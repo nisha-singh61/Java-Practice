8406eb5923b37145d8423a132b1c9ac489daa8aa
Nisha <nishakuswaha2002@gmail.com>
2026-03-30 20:44:15 +0530

    Division Operator with float or double and int variable

---
+++ src/basics/operator/arithematic/DivisionOperator2.java
@@ -0,0 +1,15 @@
+package basics.operator.arithematic;
+
+public class DivisionOperator2 {
+    public static void main(String[] args) {
+        int x = 100;
+
+        float y = .25f, division;
+
+        //In this arithmetic operation, one of the variable is of float
+        //type so the 'division' variable is declared 'float' type.
+        division = x/y;
+        System.out.println("Quotient value after dividing x by y: "+division);
+    }
+
+}
