20413a0cf2b6a923ce75cada88f4e029f005d304
Nisha Singh Kushwaha <nishakuswaha2002@gmail.com>
2026-03-31 19:04:19 +0530

    Equal Operator

---
+++ src/basics/operator/assignment/EqualOperator.java
@@ -0,0 +1,20 @@
+package basics.operator.assignment;
+
+public class EqualOperator {
+    public static void main(String[] args)
+    {
+        int n; //integer variable
+        char ch; //character variable
+        String str; //string variable
+
+        // Simple assignment operator to assign values to variables
+        n = 1;
+        ch = 'A';
+        str = "Nisha Singh";
+
+        // Displaying the values of all the variables
+        System.out.println("The value assigned to 'n': " + n);
+        System.out.println("The value assigned to 'ch': " + ch);
+        System.out.println("The value assigned to 'str': " + str);
+    }
+}
