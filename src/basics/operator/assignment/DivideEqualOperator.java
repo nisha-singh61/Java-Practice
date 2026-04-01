8dc43044735dc0466aad9e441d9a1629cf7b71f1
Nisha Singh Kushwaha <nishakuswaha2002@gmail.com>
2026-04-01 18:53:54 +0530

    /= Operator

---
+++ src/basics/operator/assignment/DivideEqualOperator.java
@@ -0,0 +1,16 @@
+package basics.operator.assignment;
+
+public class DivideEqualOperator {
+    public static void main(String[] args)
+    {
+        //declaring integer variables
+        int x=100, y =20;
+
+        //Operator /= used here to divide the variable x by
+        // variable y and assign the quotient to x
+        x /=y;
+
+        System.out.println("Value of variable x: "+x);
+        System.out.println("Value of variable y: "+y);
+    }
+}
