80874357c9c5bf37e927d5f1c28993496a0cac57
Nisha Singh Kushwaha <nishakuswaha2002@gmail.com>
2026-04-01 18:48:28 +0530

    *= Operator

---
+++ src/basics/operator/assignment/MultiplyEqualOperator.java
@@ -0,0 +1,16 @@
+package basics.operator.assignment;
+
+public class MultiplyEqualOperator {
+    public static void main(String[] args)
+    {
+        //declaring integer variables
+        int num1=10, num2 =20;
+
+        //Operator *= used here to multiply the num1 by num2
+        //and assign the result to num1
+        num1 *=num2;
+
+        System.out.println(num1);
+        System.out.println(num2);
+    }
+}
