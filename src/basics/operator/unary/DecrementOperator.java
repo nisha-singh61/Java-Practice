a374a6c22f508ee3a025c9a1e5908411ae64c9fc
Nisha Singh Kushwaha <nishakuswaha2002@gmail.com>
2026-04-05 19:31:45 +0530

    Decrement Operator

---
+++ src/basics/operator/unary/DecrementOperator.java
@@ -0,0 +1,12 @@
+package basics.operator.unary;
+
+public class DecrementOperator {
+    public static void main(String[] args) {
+        int number = 5;
+
+        //decrement unary operator
+        number--;
+
+        System.out.println(number);
+    }
+}
