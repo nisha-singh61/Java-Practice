b9c0d2daed134ea2b1ad0bd865c8e40b05d05826
Nisha Singh Kushwaha <nishakuswaha2002@gmail.com>
2026-04-05 19:28:59 +0530

    Increment ++ Operator

---
+++ src/basics/operator/unary/IncrementOperator.java
@@ -0,0 +1,12 @@
+package basics.operator.unary;
+
+public class IncrementOperator {
+    public static void main(String[] args) {
+        int number = 5;
+
+        //increment unary operator
+        number++;
+
+        System.out.println(number);
+    }
+}
