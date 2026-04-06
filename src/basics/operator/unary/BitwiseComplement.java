2a7a7d2076c2aac56a0fb1d7b87346e249c99837
Nisha Singh Kushwaha <nishakuswaha2002@gmail.com>
2026-04-06 20:33:34 +0530

    Bitwise Complement Operator

---
+++ src/basics/operator/unary/BitwiseComplement.java
@@ -0,0 +1,10 @@
+package basics.operator.unary;
+
+public class BitwiseComplement {
+    public static void main(String[] args) {
+        int num = 7;
+
+        //print bitwise complement of 7
+        System.out.println(~num);
+    }
+}
