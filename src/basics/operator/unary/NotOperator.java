ce43239efabb6ff09cb334eacba6da98e20c8acd
Nisha Singh Kushwaha <nishakuswaha2002@gmail.com>
2026-04-06 20:29:14 +0530

    Not Operator

---
+++ src/basics/operator/unary/NotOperator.java
@@ -0,0 +1,16 @@
+package basics.operator.unary;
+
+public class NotOperator {
+    public static void main(String[] args) {
+        int totalMarks = 32;
+
+        //if total marks are not greater than 33 then
+        //the student is fail else pass
+        if(!(totalMarks > 33)){
+            System.out.println("Fail");
+        }
+        else{
+            System.out.println("Pass");
+        }
+    }
+}
