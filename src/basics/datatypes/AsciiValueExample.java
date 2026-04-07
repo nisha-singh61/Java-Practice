63ed801ce554668b47f6e0cd1e6ae57763f14dc2
Nisha Singh Kushwaha <nishakuswaha2002@gmail.com>
2026-04-07 23:11:45 +0530

    ASCII Values

---
+++ src/basics/datatypes/AsciiValueExample.java
@@ -0,0 +1,14 @@
+package basics.datatypes;
+
+public class AsciiValueExample {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+
+        char c = 'a';
+        char bb = 'k';
+
+        System.out.println(c);
+        System.out.println((int)c);
+    }
+}
