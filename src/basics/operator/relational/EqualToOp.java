fcf299b2ad17e2275471f9fa20c7fcc277243b35
Nisha Singh Kushwaha <nishakuswaha2002@gmail.com>
2026-04-11 20:57:35 +0530

    Equal To Operator

---
+++ src/basics/operator/relational/EqualToOp.java
@@ -0,0 +1,15 @@
+package basics.operator.relational;
+
+public class EqualToOp {
+    public static void main(String[] args) {
+        int num1 = 9, num2 = 9;
+
+        // == Operator is used to check whether num1 and num2 are equal or not
+        if(num1 == num2){
+            System.out.println("Numbers num1 and num2 are equal");
+        }
+        else{
+            System.out.println("Numbers num1 and num2 are not equal");
+        }
+    }
+}
