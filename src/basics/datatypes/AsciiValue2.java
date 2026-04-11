c501e19c654bc838a031e1122a09340d35197539
Nisha Singh Kushwaha <nishakuswaha2002@gmail.com>
2026-04-11 19:19:53 +0530

    ASCII Values

---
+++ src/basics/datatypes/AsciiValue2.java
@@ -0,0 +1,15 @@
+package basics.datatypes;
+
+public class AsciiValue2 {
+    static char c;
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 10;
+
+        char bb = 'k';
+
+        System.out.println((int)c);
+        System.out.println(c);
+    }
+
+}
