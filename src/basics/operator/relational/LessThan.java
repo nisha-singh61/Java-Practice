7864a9d9917a14cf10c7525d3402b6509911a446
Nisha Singh Kushwaha <nishakuswaha2002@gmail.com>
2026-04-13 19:49:25 +0530

    Less Than

---
+++ src/basics/operator/relational/LessThan.java
@@ -0,0 +1,13 @@
+package basics.operator.relational;
+
+public class LessThan {
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 20, num3 = 5;
+
+        //checking whether num1 is less than num2 using '<' operator
+        System.out.println("num1 is less than num2: "+(num1<num2));
+
+        //checking whether num1 is less than num3 using '<' operator
+        System.out.println("num1 is less than num3: "+(num1<num3));
+    }
+}
