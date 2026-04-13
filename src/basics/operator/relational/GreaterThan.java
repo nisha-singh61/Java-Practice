92aadcd8b47a6f409590de14086462c2ea8fb903
Nisha Singh Kushwaha <nishakuswaha2002@gmail.com>
2026-04-13 19:46:19 +0530

    Greater Than

---
+++ src/basics/operator/relational/GreaterThan.java
@@ -0,0 +1,13 @@
+package basics.operator.relational;
+
+public class GreaterThan {
+    public static void main(String[] args) {
+        int num1 = 10, num2 = 20, num3 = 5;
+
+        //checking whether num1 > num2
+        System.out.println("num1 is greater than num2: "+(num1>num2));
+
+        //checking whether num1 > num3
+        System.out.println("num1 is greater than num3: "+(num1>num3));
+    }
+}
