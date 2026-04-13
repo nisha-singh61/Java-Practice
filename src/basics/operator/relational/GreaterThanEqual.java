5c4dd18f9eae7fa6a0e06ccef0a34ad708bf620b
Nisha Singh Kushwaha <nishakuswaha2002@gmail.com>
2026-04-13 19:48:42 +0530

    Greater Than or equal

---
+++ src/basics/operator/relational/GreaterThanEqual.java
@@ -0,0 +1,19 @@
+package basics.operator.relational;
+
+public class GreaterThanEqual {
+    public static void main(String[] args) {
+        //declaring integer variables
+        int num1, num2;
+
+        //assigning values to variables using
+        //assignment operator
+        num1 = 100;
+        num2 = 100;
+
+        //Comparing using > operator
+        System.out.println("num1 > num2: "+(num1>num2));
+
+        //Comparing using >= operator
+        System.out.println("num1 >= num2: "+(num1>=num2));
+    }
+}
