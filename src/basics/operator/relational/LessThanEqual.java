cd7db843f320f3e1ac530145fac580ccb86d29dc
Nisha Singh Kushwaha <nishakuswaha2002@gmail.com>
2026-04-15 20:31:55 +0530

    Less Than Equal to

---
+++ src/basics/operator/relational/LessThanEqual.java
@@ -0,0 +1,13 @@
+package basics.operator.relational;
+
+public class LessThanEqual {
+    public static void main(String[] args) {
+        int x = 100, y = 150, z = 100;
+
+        //checking whether x is less than or equal to y
+        System.out.println("x is less than or equal to y: "+(x<=y));
+
+        //checking whether x is less than or equal to z
+        System.out.println("x is less than or equal to z: "+(x<=z));
+    }
+}
