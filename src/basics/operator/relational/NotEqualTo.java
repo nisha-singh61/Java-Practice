c06081ceb36ecc4c7231922cce809f13ca49b897
Nisha Singh Kushwaha <nishakuswaha2002@gmail.com>
2026-04-15 20:32:52 +0530

    Not Equal to

---
+++ src/basics/operator/relational/NotEqualTo.java
@@ -0,0 +1,13 @@
+package basics.operator.relational;
+
+public class NotEqualTo {
+    public static void main(String[] args) {
+        int x = 100, y = 150, z = 100;
+
+        //checking whether x is not equal to y
+        System.out.println("x is not equal to y: "+(x!=y));
+
+        //checking whether x is not equal to z
+        System.out.println("x is not equal to z: "+(x!=z));
+    }
+}
