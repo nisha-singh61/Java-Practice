500afdb54da7b820757b32d26781f87044b1f4b0
Nisha Singh Kushwaha <nishakuswaha2002@gmail.com>
2026-04-23 19:53:50 +0530

    Bitwise OR Operator

---
+++ src/basics/operator/bitwise/BitwiseOr.java
@@ -0,0 +1,10 @@
+package basics.operator.bitwise;
+
+public class BitwiseOr {
+    public static void main(String[] args) {
+        int num1 = 6, num2 = 10;
+
+        //Bitwise OR of 6 and 10
+        System.out.println("Value of num1|num2: "+ (num1|num2));
+    }
+}
