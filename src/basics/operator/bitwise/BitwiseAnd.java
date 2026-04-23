3fa7c301cdbd67445a6e1e5deb51bae236435d6e
Nisha Singh Kushwaha <nishakuswaha2002@gmail.com>
2026-04-23 19:52:29 +0530

    Bitwise AND Operator

--- src/basics/operator/bitwise/BitwiseAnd.java
+++ src/basics/operator/bitwise/BitwiseAnd.java
@@ -5,6 +5,6 @@ public class BitwiseAnd {
         int num1 = 6, num2 = 10;
 
         //Bitwise AND Operation on two integers
-        System.out.println("Value of num1 & num2: "+ (num1 & num2));
+        System.out.println("Value of num1 & num2: "+ (num1 & num2)); 
     }
 }
