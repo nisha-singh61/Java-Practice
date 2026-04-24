a5448cb6d77b415be753f855a86029915d004845
Nisha Singh Kushwaha <nishakuswaha2002@gmail.com>
2026-04-24 19:52:44 +0530

    Data Types

---
+++ src/basics/datatypes/DataTypes.java
@@ -0,0 +1,31 @@
+package basics.datatypes;
+
+public class DataTypes {
+    public static void main(String[] args) {
+        byte age = 13;  //-128 to 127
+        System.out.println(age);
+
+        short marks = 90;
+        System.out.println(marks);
+
+        int salary = 600000;
+        System.out.println(salary);
+
+        long num = 123456789123456789L;
+        System.out.println(num);
+
+        System.out.println("--------------------------------------------");
+
+        float temp = 103.4f;
+        System.out.println(temp);
+
+        double pi = 3.14159;
+        System.out.println(pi);
+
+        char grade = 'A';
+        System.out.println(grade);
+
+        boolean flag = true;
+        System.out.println(flag);
+    }
+}
